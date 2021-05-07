0863cb8c1b7978d198905bd20a3e014637691bae
Shrikant <Shrikantdandile@gamil.com>
2021-05-07 09:59:43 +0530

    Method Implementation

---
+++ CleanArch/CleanArch.Data/Repository/CourseRepository.cs
@@ -0,0 +1,25 @@
+﻿using CleanArch.Data.Context;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Data.Repository
+{
+   public class CourseRepository : ICourseRepository
+   {
+      private readonly UniversityDBContext _dataContext;
+
+      public CourseRepository(UniversityDBContext dataContext)
+      {
+         _dataContext = dataContext;
+      }
+      public IEnumerable<Course> GetCourses()
+      {
+         return _dataContext.Courses;
+      }
+   }
+}
